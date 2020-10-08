67eec6b65dbead26a4ff52579d0621e1f5f2d405
gutovesco <guto.vesco@hotmail.com>
2020-10-08 11:55:30 -0300

    added alt

--- src/App.js
+++ src/App.js
@@ -32,15 +32,15 @@ function App() {
         <h2 style={{ marginTop: 100 }}>Quem somos nós</h2>
         <div style={{ display: 'flex', flexDirection: 'row', marginTop: 20, marginBottom: 100 }}>
           <div style={{ marginTop: 20, marginRight: 30, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <img style={{ width: 200, height: 200, borderRadius: 100 }} src="https://media-exp1.licdn.com/dms/image/C4D03AQFAfzHMDRyL2Q/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=feLdyYhL93kXrMiQTM3QpAjXWJLcvYzUSTrvnZG3rY8" />
+            <img alt="" style={{ width: 200, height: 200, borderRadius: 100 }} src="https://media-exp1.licdn.com/dms/image/C4D03AQFAfzHMDRyL2Q/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=feLdyYhL93kXrMiQTM3QpAjXWJLcvYzUSTrvnZG3rY8" />
             <span style={{ marginTop: 20, width: 300, height: 200 }}>Augusto é desenvolvedor de software na Compasso UOL, possui conhecimento nas linguagens Javascript e Dart. Programa usando as tecnologias React, React Native e Flutter. Fluente em inglês com certificação TOEIC.</span>
           </div>
           <div style={{ marginTop: 20, marginRight: 30, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <img style={{ width: 200, height: 200, borderRadius: 100 }} src="https://i.ibb.co/kJdb5Ms/Screenshot-4.png" />
+            <img alt="" style={{ width: 200, height: 200, borderRadius: 100 }} src="https://i.ibb.co/kJdb5Ms/Screenshot-4.png" />
             <span style={{ marginTop: 20, width: 300, height: 200 }}>Lucas é gerente na loja Flores na mão e trabalha no help desk da empresa SCJ. Possui experiência na área de negócios e de manutenção de computadores.</span>
           </div>
           <div style={{ marginTop: 20, marginRight: 30, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-            <img style={{ width: 200, height: 200, borderRadius: 100 }} src="https://media-exp1.licdn.com/dms/image/C4D03AQFE7z3GfjHWUw/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=MaBjXNcrTu_bb9IUkUGMvc1ubEZiMQrhSH4d84c7Ba4" />
+            <img alt="" style={{ width: 200, height: 200, borderRadius: 100 }} src="https://media-exp1.licdn.com/dms/image/C4D03AQFE7z3GfjHWUw/profile-displayphoto-shrink_200_200/0?e=1607558400&v=beta&t=MaBjXNcrTu_bb9IUkUGMvc1ubEZiMQrhSH4d84c7Ba4" />
             <span style={{ marginTop: 20, width: 300, height: 200 }}>Samuel é desenvolvedor de software na compasso UOL com foco nas tecnologias React, React Native e Flutter. Possui experiência atuando em projetos e-commerce com magento.</span>
           </div>
         </div>
